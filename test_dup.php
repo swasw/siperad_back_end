887d70b344a84910c0fc0e4824a67721764c4528
swasw <aqilaswangga1@gmail.com>
2026-07-01 22:43:33 +0700

    minor changes

--- test_dup.php
+++ test_dup.php
@@ -4,5 +4,5 @@ $app = require_once __DIR__.'/bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 $kernel->bootstrap();
 
-$konfirmasis = App\Models\JadwalKonfirmasi::where('jadwal_ruangan_id', 1)->get();
-echo json_encode($konfirmasis->toArray(), JSON_PRETTY_PRINT);
+$konfirmasis = App\Models\JadwalKonfirmasi::with('jadwalRuangan')->get();
+file_put_contents('output2.json', json_encode($konfirmasis->toArray(), JSON_PRETTY_PRINT));
